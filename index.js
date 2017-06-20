21f2784693a0c0a60f9a4b707a3a2dc86d7a825a
Marce <marce200x@gmail.com>
2017-06-20 01:36:03 -0300

    Adding support for strings with any original case

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 module.exports = function (string) {
+  string = string.toLowerCase();
   return string.charAt(0).toUpperCase() + string.substring(1);
 }
 
 module.exports.words = function (string) {
-  return string.replace(/(^|[^a-zA-Z\u00C0-\u017F'])([a-zA-Z\u00C0-\u017F])/g, function (m) {
+  return string.toLowerCase().replace(/(^|[^a-zA-Z\u00C0-\u017F'])([a-zA-Z\u00C0-\u017F])/g, function (m) {
     return m.toUpperCase()
   })
 }
