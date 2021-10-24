23db2dc2b5683c8f6ee0441caf29577873e2f842
Stephen Sugden <grncdr@users.noreply.github.com>
2021-10-24 14:44:49 +0200

    Allow capitalize.words to accept an options object

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-module.exports = function (string, preserve) {
-  if (!preserve) {
+
+function capitalize (string, opts) {
+  opts = normalizeOptions(opts)
+  if (!opts.preserve) {
     string = string.toLowerCase();
   }
   return string.charAt(0).toUpperCase() + string.substring(1);
 }
 
-module.exports.words = function (string, preserve) {
-  if (!preserve) {
+capitalize.words = function (string, opts = {}) {
+  opts = normalizeOptions(opts)
+  if (!opts.preserve) {
     string = string.toLowerCase();
   }
   return string.replace(/(?!^[0-9])(^|[^a-zA-Z\u00C0-\u017F\u0400-\u04FF'])([a-zA-Z\u00C0-\u017F\u0400-\u04FF])/g, function (m) {
     return m.toUpperCase()
   })
 }
+
+function normalizeOptions(opts) {
+  if (!opts) {
+    return { preserve: false }
+  }
+  if (typeof opts === 'boolean') {
+    return { preserve: opts }
+  }
+  return opts || {}
+}
+
+module.exports = capitalize
