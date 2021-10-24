cee5a68b3ef9264ef0b9838722de5e7ce116840d
Stephen Sugden <me@stephensugden.com>
2021-10-24 16:52:03 +0200

    Split words, then capitalize

--- index.js
+++ index.js
@@ -7,14 +7,25 @@ function capitalize (string, opts) {
   return string.charAt(0).toUpperCase() + string.substring(1);
 }
 
-capitalize.words = function (string, opts = {}) {
+capitalize.words = function (string, opts) {
   opts = normalizeOptions(opts)
   if (!opts.preserve) {
     string = string.toLowerCase();
   }
-  return string.replace(/(?!^[0-9])(^|[^a-zA-Z\u00C0-\u017F\u0400-\u04FF'])([a-zA-Z\u00C0-\u017F\u0400-\u04FF])/g, function (m) {
-    return m.toUpperCase()
-  })
+  var start = 0
+  var nonWord = /[^0-9a-zA-Z\u00C0-\u017F\u0400-\u04FF']+|$/g
+  var match
+  var out = ""
+
+  while (match = nonWord.exec(string)) {
+    var suffix = match[0]
+    var word = string.substring(start, nonWord.lastIndex - suffix.length)
+    out += capitalize(word, opts) + suffix
+    start = nonWord.lastIndex
+    if (start == string.length) break
+  }
+
+  return out
 }
 
 function normalizeOptions(opts) {
